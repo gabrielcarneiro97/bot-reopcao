179a25c13b6d3ae208e9134c237019686dcd9c13
Gabriel Carneiro <gabriel.carneiro.castro@gmail.com>
2020-10-19 13:12:17 -0300

    adicionei um contador

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const timers = require('timers');
 const alert = require('alert');
 
 
+let counter = 0;
+
 async function check() {
-  const browser = await pptr.launch();
-  const page = await browser.newPage();
+  counter += 1;
 
-  await page.goto('https://www2.ufmg.br/drca/drca/Home/Graduacao/Reopcao-de-Curso/Resultado-dos-Colegiados-de-Cursos');
+  try {
+    const browser = await pptr.launch();
+    const page = await browser.newPage();
 
-  const res = await page.evaluate(() => {
-    const arr = Array.from(document.getElementsByClassName('attribute-long')[0].children);
-    const cc = arr.find((el) => el.innerText.includes('Computação'));
-    return cc.innerHTML.includes('href');
-  });
+    await page.goto('https://www2.ufmg.br/drca/drca/Home/Graduacao/Reopcao-de-Curso/Resultado-dos-Colegiados-de-Cursos');
 
-  browser.close();
+    const res = await page.evaluate(() => {
+      const arr = Array.from(document.getElementsByClassName('attribute-long')[0].children);
+      const cc = arr.find((el) => el.innerText.includes('Computação'));
+      return cc.innerHTML.includes('href');
+    });
 
-  return res;
+    await browser.close();
+
+    console.log(`A consulta ${counter} cabo.`);
+    return res;
+  } catch (err) {
+    console.log(`Eitcha rapaziada deu um biziu aqui na consulta ${counter}.`);
+    return false;
+  }
 }
 
 timers.setInterval(async () => {
